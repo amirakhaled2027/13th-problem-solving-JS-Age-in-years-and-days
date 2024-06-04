42ddcdbcf6df9d2ec694cf61f169dc2e28b7fa15
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-06-05 01:10:43 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//Create a function that takes the age in years and returns the age in days and print it on console
+function ageInDays(years){
+    var age = years * 365;
+    return age;
+  }
+  console.log(ageInDays(23));
